783e251995beafe24a7fda2b7ef72d6b8f60dc18
JeffVenancius <43701418+JeffVenancius@users.noreply.github.com>
2024-11-25 15:58:55 -0300

    Update index.jsx

--- src/components/Buttons/wppBtn/index.jsx
+++ src/components/Buttons/wppBtn/index.jsx
@@ -1,8 +1,10 @@
+import './wppButton.css'
+
 function WppBtn(props) {
 	const wppIcon = <img src="./assets/assis/icone4.svg" style={{height:"70%", marginRight: "2%"}}/>
 	return (
 				<a href={"https://wa.me/553598652571?text=" + encodeURIComponent(props.mensagem)} target="_blank" rel="noreferrer" style={{textDecoration: "none", width:props.btnStyle.width, display:"flex", justifyContent:"center", alignItems: "center"}}>
-			<button style={{...props.btnStyle, width:"100%"}}>{props.icone && wppIcon}{props.texto}</button>
+			<button style={{...props.btnStyle, width:"100%"}} className="wppButton">{props.icone && wppIcon}{props.texto}</button>
 		</a>
 	)
 }
